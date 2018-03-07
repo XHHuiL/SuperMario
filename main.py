9e2f297f007cc5ea79407b431cf8094ffe25ceab
XHHuiL <16302010048@fudan.edu.cn>
2018-03-07 14:17:44 +0800

    modify the main.py to display a image

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python
 __author__ = "liuhui"
+import pygame as py
+from pygame.locals import *
+
+
+# init
+py.init()
+
+# # the title
+title = "Super Mario"
+py.display.set_caption(title)
+
+# # the screen
+scr_size = (200, 200)
+scr = py.display.set_mode(scr_size, 0, 32)
+
+# # the test mario image
+mario_jump_path = "resources/graphics/mariojump2.gif"
+mario_jump = py.image.load(mario_jump_path).convert_alpha()
 
 
 def main():
+    while True:
+
+        for event in py.event.get():
+            if event.type == QUIT:
+                quit(1)
+
+        scr.blit(mario_jump, (0, 0))
+        py.display.update()
     pass
 
 
